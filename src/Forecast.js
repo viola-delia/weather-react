e6daefee47f5555e5d5ec5335f3d7fac636ba28e
viola-delia <viola.delia28@gmail.com>
2024-06-22 11:03:16 +0800

    Update CSS

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,27 +1,57 @@
 import React from "react";
 import axios from 'axios';
+import forecast from './Forecast.css';
 
 
 export default function Forecast() {
     return (
         <div className='content'>
+            
             <form>
-                <input type='text'></input>
-                <input type='submit' value='Search'></input>
+                <div className='formElements'>
+                <input type='text' className='searchBar' placeholder="Enter a City"></input>
+                <input type='submit' value='Search' className='searchButton'></input>
+                </div>
                 <br />
+                <div className='mainCity'>
                 <h1>San Francisco</h1>
-                <h2>☁️ 11° C</h2>
+                <h2> <span className='mainCityTemperature'>☁️11</span>° C</h2>
+                </div>
                 <p>Friday 16:17, overcast clouds</p>
-                <p>Humidity: 90%, Wind: 7.15km/h</p>
+                <p>Humidity: <span className='humidityPercentage'>90%</span>, Wind: <span className='wind'>7.15km/h</span></p>
                 <p>
-                    <ul>
-                        <li>Sat</li>
-                        <li>Sun</li>
-                        <li>Mon</li>
-                        <li>Tue</li>
-                        <li>Wed</li>
-                        <li>Thu</li>
-                    </ul>
+                <div class="weather-container">
+  <div class="weather-day">
+    <div>Sat</div>
+    <div class="weather-emoji">🌤️</div>
+    <div class="weather-temperature">16° 12°</div>
+  </div>
+  <div class="weather-day">
+    <div>Sun</div>
+    <div class="weather-emoji">🌤️</div>
+    <div class="weather-temperature">16° 12°</div>
+  </div>
+  <div class="weather-day">
+    <div>Mon</div>
+    <div class="weather-emoji">🌤️</div>
+    <div class="weather-temperature">16° 12°</div>
+  </div>
+  <div class="weather-day">
+    <div>Tue</div>
+    <div class="weather-emoji">🌤️</div>
+    <div class="weather-temperature">16° 12°</div>
+  </div>
+  <div class="weather-day">
+    <div>Wed</div>
+    <div class="weather-emoji">🌤️</div>
+    <div class="weather-temperature">16° 12°</div>
+  </div>
+  <div class="weather-day">
+    <div>Thu</div>
+    <div class="weather-emoji">🌤️</div>
+    <div class="weather-temperature">16° 12°</div>
+  </div>
+</div>
                 </p>
             </form>
         </div>
